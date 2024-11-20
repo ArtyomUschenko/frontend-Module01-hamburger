42c260bbbf4f14ea61573f6832d71af66bad4d0e
Artyom Uschenko <net775@yandex.ru>
2024-11-20 14:24:06 +0300

    Плавный скролл до следующего блока

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// Smooth Scroll
+
+const links = document.querySelectorAll(".banner-btn");
+
+for (const link of links) {
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+// Button Up
+
