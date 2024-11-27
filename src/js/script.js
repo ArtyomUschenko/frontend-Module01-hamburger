24ecd4495a8b7ca64405b4222cc0a34868ff56c1
Artyom Uschenko <net775@yandex.ru>
2024-11-27 17:27:00 +0300

    Создания модального окна для планшетов

--- src/js/script.js
+++ src/js/script.js
@@ -44,3 +44,12 @@ function scrollFunction(){
 
 
 
+// Menu
+
+function openModal() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-400px";
+}
